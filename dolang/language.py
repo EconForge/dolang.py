b449ac207f0173f4e3f91be833f20b013bc9a672
Winant Pablo <pablo.winant@gmail.com>
2020-04-09 00:51:14 +0200

    REF: Moved from Dolang.jl

---
+++ dolang/language.py
@@ -0,0 +1,21 @@
+import math
+
+functions = {
+    'log': math.log,
+    'exp': math.exp,
+    'sin': math.sin,
+    'cos': math.cos,
+    'tan': math.tan,
+    'atan': math.atan,
+    'tanh': math.tanh,
+    'atanh': math.atanh,
+    'min': min,
+    'max': max,
+    'minimum': min,
+    'maximum': max,
+    'abs': abs
+}
+
+constants = {
+    'pi': math.pi,
+}
