07c92b2ca7463381268dc43c6cb2ef1e39e9076e
Pablo Winant <pablo.winant@gmail.com>
2020-09-22 17:03:06 +0200

    Added __version__ string.

--- dolang/__init__.py
+++ dolang/__init__.py
@@ -1,3 +1,5 @@
+from dolang.version import __version_info__, __version__
+
 from dolang.symbolic import parse_string
 from dolang.symbolic import stringify, list_variables, list_symbols, time_shift, steady_state
 from dolang.grammar import str_expression
